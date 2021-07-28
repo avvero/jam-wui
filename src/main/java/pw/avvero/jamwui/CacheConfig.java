443fba644d291106621fd87039e9768baa0e1e11
fxdev-belyaev-ay <anton.belyaev@sibit.tech>
2021-07-29 00:17:34 +0700

    Caching for dependencies and d3 render

---
+++ src/main/java/pw/avvero/jamwui/CacheConfig.java
@@ -0,0 +1,9 @@
+package pw.avvero.jamwui;
+
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Configuration;
+
+@EnableCaching
+@Configuration
+public class CacheConfig {
+}
--- src/main/java/pw/avvero/jamwui/RestController.java
+++ src/main/java/pw/avvero/jamwui/RestController.java
@@ -1,17 +1,25 @@
 package pw.avvero.jamwui;
 
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cache.Cache;
+import org.springframework.cache.CacheManager;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import pw.avvero.jam.JamService;
+import pw.avvero.jam.core.Issue;
+import pw.avvero.jam.core.IssueLink;
+import pw.avvero.jam.graphviz.GraphvizWriter;
 
+@Slf4j
 @Controller
 @AllArgsConstructor
 public class RestController {
 
-    private JamService jamService;
+    private final JamService jamService;
+    private final CacheManager cacheManager;
 
     @GetMapping("/checkout")
     public String checkout(@RequestParam String issueCode, Model model) {
@@ -21,9 +29,31 @@ public class RestController {
     }
 
     @GetMapping("/dependencies")
-    public String dependencies(@RequestParam String issueCode, Model model) {
-        String dependencyGraph = jamService.getDependenciesForIssueWithChildren(issueCode);
-        model.addAttribute("dependencyGraph", dependencyGraph);
-        return "dependencies";
+    public String dependencies(@RequestParam String issueCode,
+                               @RequestParam(required = false) String render,
+                               Model model) {
+        Cache cache = cacheManager.getCache("dependencies");
+        String issueDependencies = cache.get(issueCode, String.class);
+        if (issueDependencies == null) {
+            Issue issue = jamService.getIssueWithChildren(issueCode);
+            // TODO custom jira structure specifique
+            if ("Initiative".equalsIgnoreCase(issue.getType()) && issue.getLinks() != null) {
+                for (IssueLink link : issue.getLinks()) {
+                    if ("is parent task of".equals(link.getType())) {
+                        log.debug("Take details from linked issue: {} {}", link.getType(), link.getIssue().getKey());
+                        Issue enriched = jamService.getIssueWithChildren(link.getIssue().getKey());
+                        issue.getChildren().add(enriched);
+                    }
+                }
+            }
+            issueDependencies = GraphvizWriter.toString(issue);
+            cache.put(issueCode, issueDependencies);
+        }
+        model.addAttribute("dependencyGraph", issueDependencies);
+        if ("d3".equals(render)) {
+            return "dependencies-d3";
+        } else {
+            return "dependencies";
+        }
     }
 }
